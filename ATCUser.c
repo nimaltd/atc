0bb78b506ed65f3d2a8b897ff04c2e79e4ef6079
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2019-02-07 11:19:37 +0330

    Update ATCUser.c

--- ATCUser.c
+++ ATCUser.c
@@ -1,11 +1,11 @@
 
 #include "ATC.h"
-#include "Bluetooth.h"
+//#include "Bluetooth.h"
 
 
 void		ATC_User_AutoSearchCallBack(ATC_t *atc,uint16_t	FoundIndex,char *FoundString,char *ATC_rxDataPointer)
 {
-	if(strcmp(FoundString,"+BTCONNECT:")==0)
+/*	if(strcmp(FoundString,"+BTCONNECT:")==0)
 	{
 		printf("Conected......................\r\n");
 		Bluetooth.ConnectionStatus=1;
@@ -20,6 +20,6 @@ void		ATC_User_AutoSearchCallBack(ATC_t *atc,uint16_t	FoundIndex,char *FoundStri
 	else if(strcmp(FoundString,"+BTCONNECTING:")==0)
 	{
 		Bluetooth.ConnectingNewProfile=1;
-	}
+	}*/
 }
 
