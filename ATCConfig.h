67a096e10467563554b44304babda94b9dfd4aa2
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2020-12-12 12:19:26 +0330

    Delete ATCConfig.h

--- ATCConfig.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ATCCONFIG_H
-#define _ATCCONFIG_H
-
-#define			_ATC_DEBUG                                    0   // 0: disable      1:debug        2: Print all rx data
-#define			_ATC_MAX_DEVICE                               1
-#define			_ATC_MAX_SEARCH_PARAMETER_FOR_AT_ANSWER       10
-#define			_ATC_MAX_AUTO_SEARCH_STRING                   20
-
-#endif
