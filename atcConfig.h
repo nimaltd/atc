4fc98973c927bd601a9ea0a9144a6fe055f490bc
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2020-12-19 09:43:05 +0330

    Update atcConfig.h

--- atcConfig.h
+++ atcConfig.h
@@ -2,8 +2,8 @@
 #ifndef _ATCCONFIG_H_
 #define _ATCCONFIG_H_
 
-#define	_ATC_DEBUG            1       //  use printf debug
-#define	_ATC_RTOS             1       //  0: no rtos    1: cmsis_os v1    2: cmsis_os v2
+#define	_ATC_DEBUG            0       //  use printf debug
+#define	_ATC_RTOS             0       //  0: no rtos    1: cmsis_os v1    2: cmsis_os v2
 #define	_ATC_RXSIZE           1024    //  at-command rx buffer size
 #define	_ATC_SEARCH_CMD_MAX   5       //  maximum of answer in at-command
 #define	_ATC_SEARCH_MAX       10      //  maximum	of always search in buffer
